7495efd0a12f59f24ae8bf3946a04e7491070006
Work-Desktop <ilya.a.levashov@gmail.com>
2022-11-27 14:42:33 +0300

    Fist commit

---
+++ main.py
@@ -0,0 +1,42 @@
+from PyQt5 import uic
+from PyQt5.QtGui import QColor, QPainter
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+import random
+
+
+SCREEN_SIZE = [680, 480]
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('ui.ui', self)
+        self.flag = False
+        self.pushButton.clicked.connect(self.draw)
+        self.coords = []
+
+    def draw(self):
+        self.figure = 'circle'
+        self.size = random.randint(10, 100)
+        self.color = (255, 255, 0)
+        self.flag = True
+        self.update()
+
+    def paintEvent(self, event):
+        if self.flag:
+            qp = QPainter()
+            qp.begin(self)
+            qp.setPen(QColor(*self.color))
+            qp.setBrush(QColor(*self.color))
+            self.x, self.y = random.randint(100, SCREEN_SIZE[0] - 100), random.randint(100, SCREEN_SIZE[1] - 100)
+            if self.figure == 'circle':
+                qp.drawEllipse(self.x, self.y, self.size, self.size)
+            qp.end()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec_())
